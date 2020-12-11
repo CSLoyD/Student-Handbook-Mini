4fbea79f3aa4a44979cdcd9037731e0a47235f7f
nikko <nikxuz@gmail.com>
2020-12-11 21:13:09 +0800

    nikko up --

--- app/src/main/java/com/example/finalstudenthandbook/MainActivity.kt
+++ app/src/main/java/com/example/finalstudenthandbook/MainActivity.kt
@@ -6,13 +6,9 @@ import com.github.barteksc.pdfviewer.PDFView
 
 class MainActivity : AppCompatActivity() {
 
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
 
     }
 }
