2f09b4c12296c3d0692fa4ca77dc4066308103c4
nikko <nikxuz@gmail.com>
2020-12-11 19:54:51 +0800

    nikko up

--- app/src/main/java/com/example/finalstudenthandbook/MainActivity.kt
+++ app/src/main/java/com/example/finalstudenthandbook/MainActivity.kt
@@ -6,14 +6,13 @@ import com.github.barteksc.pdfviewer.PDFView
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var mPDFView:PDFView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        mPDFView = findViewById(R.id.pdfView) as PDFView
-        mPDFView.fromAsset("studenthandbook.pdf").load()
+
 
     }
 }
