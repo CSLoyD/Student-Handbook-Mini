8618db9fc09506ec50a4d3b89aadc1ae0a75b437
Chrisnil Loyd  Avila <clavila@proweaver.net>
2020-12-14 19:06:37 +0800

    Major Fixes on Registration.kt and UpdateProfile.kt - Loyd

--- app/src/main/java/com/example/finalstudenthandbook/Registration.kt
+++ app/src/main/java/com/example/finalstudenthandbook/Registration.kt
@@ -9,28 +9,33 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.vishnusivadas.advanced_httpurlconnection.PutData
 import kotlinx.android.synthetic.main.activity_registration.*
+import kotlinx.android.synthetic.main.student_profile.*
 
 class Registration : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
 
-        btnSubmit.setOnClickListener{
+        btnReg.setOnClickListener{
 
-            if( rUserName.text.trim().isNotEmpty() && rUserPass.text.trim().isNotEmpty() ) {
-                rProgress.visibility = View.VISIBLE
+            if( regUserName.text.trim().isNotEmpty() && regUserPass.text.trim().isNotEmpty() && regFirstName.text.trim().isNotEmpty() && regLastName.text.trim().isNotEmpty() ) {
+                regProgress.visibility = View.VISIBLE
 
                 val handler = Handler(Looper.getMainLooper())
                 handler.post(Runnable {
                     //Start ng pagbasa ug pag modify sa database
                     //Himuan ng mga fields
-                    val field = arrayOfNulls<String>(2)
+                    val field = arrayOfNulls<String>(4)
                     field[0] = "rUserName"
                     field[1] = "rUserPass"
+                    field[2] = "rFirstName"
+                    field[3] = "rLastName"
                     //Maghimo ng mga data
-                    val data = arrayOfNulls<String>(2)
-                    data[0] = rUserName.text.toString()
-                    data[1] = rUserPass.text.toString()
+                    val data = arrayOfNulls<String>(4)
+                    data[0] = regUserName.text.toString()
+                    data[1] = regUserPass.text.toString()
+                    data[2] = regFirstName.text.toString()
+                    data[3] = regLastName.text.toString()
                     val putData = PutData(
                         dbConnection().urldb + "StudentHandbook/signup.php",
                         "POST",
@@ -39,12 +44,12 @@ class Registration : AppCompatActivity() {
                     )
                     if (putData.startPut()) {
                         if (putData.onComplete()) {
-                            rProgress.visibility = View.GONE
+                            regProgress.visibility = View.GONE
                             val result = putData.result
                             val suc = "Sign Up Success"
                             if(result.equals(suc,true)) {
                                 Toast.makeText(applicationContext, result, Toast.LENGTH_SHORT).show()
-                                val logIntent = Intent(this@Registration, Login::class.java)
+                                val logIntent = Intent(this, Login::class.java)
                                 startActivity(logIntent)
                                 finish()
                             } else {
--- app/src/main/java/com/example/finalstudenthandbook/UpdateProfile.kt
+++ app/src/main/java/com/example/finalstudenthandbook/UpdateProfile.kt
@@ -10,7 +10,12 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.vishnusivadas.advanced_httpurlconnection.PutData
 import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_registration.*
 import kotlinx.android.synthetic.main.student_profile.*
+import kotlinx.android.synthetic.main.student_profile.rFirstName
+import kotlinx.android.synthetic.main.student_profile.rLastName
+import kotlinx.android.synthetic.main.student_profile.rUserName
+import kotlinx.android.synthetic.main.student_profile.rUserPass
 
 class UpdateProfile : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,51 +42,57 @@ class UpdateProfile : AppCompatActivity() {
 
 
         adProfUpdate.setOnClickListener {
-
+            profileProgress.visibility = View.VISIBLE
             val LoggingIn: LoggingIn = LoggingIn(this)
             val uid = LoggingIn.getUID("UID")
+            if( rUserName.text.trim().isNotEmpty() && rUserPass.text.trim().isNotEmpty() && rFirstName.text.trim().isNotEmpty() && rLastName.text.trim().isNotEmpty() ) {
+                val handler = Handler(Looper.getMainLooper())
+                handler.post {
+                    //Starting Write and Read data with URL
+                    //Creating array for parameters
+                    val field = arrayOfNulls<String>(5)
+                    field[0] = "rUserID"
+                    field[1] = "rUserName"
+                    field[2] = "rUserPass"
+                    field[3] = "rFirstName"
+                    field[4] = "rLastName"
+                    //Creating array for data
+                    val data = arrayOfNulls<String>(5)
+                    data[0] = uid.toString()
+                    data[1] = rUserName.text.toString()
+                    data[2] = rUserPass.text.toString()
+                    data[3] = rFirstName.text.toString()
+                    data[4] = rLastName.text.toString()
+                    val putData = PutData(
+                        dbConnection().urldb + "StudentHandbook/updateProfile.php",
+                        "POST",
+                        field,
+                        data
+                    )
+                    if (putData.startPut()) {
+                        if (putData.onComplete()) {
+                            profileProgress.visibility = View.GONE
+                            val result = putData.result
+                            val suc = "Profile Updated!"
+                            if (result.equals(suc, true)) {
+                                Toast.makeText(applicationContext, result, Toast.LENGTH_SHORT).show()
+                                LoggingIn.saveUsername("lUsername", rUserName.text.toString())
+                                LoggingIn.saveFirstName("LastName", rFirstName.text.toString())
+                                LoggingIn.saveLastName("LastName", rLastName.text.toString())
 
-            profileProgress.visibility = View.VISIBLE
-            val handler = Handler(Looper.getMainLooper())
-            handler.post {
-                //Starting Write and Read data with URL
-                //Creating array for parameters
-                val field = arrayOfNulls<String>(5)
-                field[0] = "rUserID"
-                field[1] = "rUserName"
-                field[2] = "rUserPass"
-                field[3] = "rFirstName"
-                field[4] = "rLastName"
-                //Creating array for data
-                val data = arrayOfNulls<String>(5)
-                data[0] = uid.toString()
-                data[1] = rUserName.text.toString()
-                data[2] = rUserPass.text.toString()
-                data[3] = rFirstName.text.toString()
-                data[4] = rLastName.text.toString()
-                val putData = PutData(
-                    dbConnection().urldb + "StudentHandbook/updateProfile.php",
-                    "POST",
-                    field,
-                    data
-                )
-                if (putData.startPut()) {
-                    if (putData.onComplete()) {
-                        profileProgress.visibility = View.GONE
-                        val result = putData.result
-                        val suc = "Profile Updated!"
-                        if (result.equals(suc, true)) {
-                            Toast.makeText(applicationContext, result, Toast.LENGTH_SHORT).show()
 
-                            val mainIntent = Intent(this, StudentDashboard::class.java)
-                            startActivity(mainIntent)
-                            finish()
-                        } else {
-                            Toast.makeText(applicationContext, result, Toast.LENGTH_SHORT).show()
+                                val mainIntent = Intent(this, StudentDashboard::class.java)
+                                startActivity(mainIntent)
+                                finish()
+                            } else {
+                                Toast.makeText(applicationContext, result, Toast.LENGTH_SHORT).show()
+                            }
                         }
                     }
+                    //End Write and Read data with URL
                 }
-                //End Write and Read data with URL
+            } else {
+                Toast.makeText(applicationContext, "Please fill out all fields!", Toast.LENGTH_SHORT).show()
             }
         }
     }
